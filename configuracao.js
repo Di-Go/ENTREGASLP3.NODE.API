3042b235458ecadabb3a4257ab7717b3769d74f0
Di-Go <alunodiego360@gmail.com>
2020-06-02 19:04:26 -0300

    MASTER - Organizando arquivo de configuração

---
+++ configuracao.js
@@ -0,0 +1,21 @@
+const bodyParser = require('body-parser');
+
+const express = require('express');
+const servidor = express();
+
+//#region Configuração dos Módulos
+    // BodyParser
+    servidor.use(bodyParser.json());
+    servidor.use(bodyParser.urlencoded({extended: false}));
+//#endregion
+
+//#region Configuração de Rotas
+
+//#endregion
+
+//#region Configuração do Servidor
+    const PORTA = 8082;
+    servidor.listen(PORTA, () => {
+        console.log('Servidor executando no endereço http://localhost:${PORTA}');
+    });
+//#endregion
