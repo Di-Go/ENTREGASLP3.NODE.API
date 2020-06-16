ce5e0758723f322feec89b7d2b8a1b0a42a2fbdd
Di-Go <alunodiego360@gmail.com>
2020-06-16 19:51:43 -0300

    MASTER - Ajustando warnings de conexão com banco de dados

--- src/database/DBFILMES.js
+++ src/database/DBFILMES.js
@@ -1,10 +1,21 @@
 const mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/DBFILMES').then(() => {
-    console.log('Servidor conectado ao Banco de Dados.');
-}).catch((err) => {
-    console.error(`Falha ao se conectar ao Banco de Dados.\n Mensagem de Erro:\n ${err}`);
-});
+//#region Configuração do Banco de Dados
+    const mongodbEnderecoLocal = 'mongodb://localhost/DBFILMES';
+    const mongodbConfiguracao = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    };
+//#endregion
+
+//#region Conexão com Banco de Dados
+    mongoose.Promise = global.Promise;
+    mongoose.connect(mongodbEnderecoLocal, mongodbConfiguracao)
+        .then(() => {
+            console.log('Servidor conectado ao Banco de Dados.');
+        }).catch((err) => {
+            console.error(`Falha ao se conectar ao Banco de Dados.\n Mensagem de Erro:\n ${err}`);
+        });
+//#endregion
 
 module.exports = mongoose;
